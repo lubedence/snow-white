c49cf9cca51260047a65b4026a2a191fff22f31e
lukas <luky_f_@hotmail.com>
2014-01-20 17:13:05 +0100

    licence header

--- src/com/tuwien/snowwhite/CameraPreview.java
+++ src/com/tuwien/snowwhite/CameraPreview.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Lukas Furlan (furlan.lukas@gmail.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Lukas Furlan - initial API and implementation
+ ******************************************************************************/
 package com.tuwien.snowwhite;
 
 import java.io.IOException;
